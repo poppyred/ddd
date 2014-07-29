064ad834c1d0529fde505b6b88162c7c538ae564
likunxiang <lkxlamborghini@126.com>
2014-07-29 22:11:47 +0800

    commit

--- release/dnspro/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#nohup python $MGRHOME/mgr.pyc > $MGRHOME/mgr.log 2>&1 &
-python $MGRHOME/mgr.pyc start
--- release/dnspro/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python $MGRHOME/mgr.pyc stop
-ps -ef | grep python | grep mgr.pyc | grep -v grep | awk '{print $2}' | xargs kill -9
-ps -ef | grep python | grep mgr.pyc | grep -v grep
