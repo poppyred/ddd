868e2b0b489372dc60c520fdeff165b5cd85536b
likunxiang <lkxlamborghini@126.com>
2014-09-05 17:15:16 +0800

    commit

--- mgr_conf.py
+++ mgr_conf.py
@@ -8,7 +8,7 @@ log_direction=1
 
 g_size_perpack = 1024
 g_row_perpack = 10
-g_row_perpack4init = 1000
+g_row_perpack4init = 500
 
 g_inner_chk_init_ok_time = 2
 g_inner_chk_snd_time = 10
@@ -19,7 +19,7 @@ g_url_inner_chk_task_ip = '121.201.12.60'
 g_url_inner_chk_task_url = 'http://' + g_url_inner_chk_task_ip + '/dnspro/dnsbroker/'
 g_mgr_sid = 'node_mgr_primary_zs_bgp'
 
-g_db_ip= '121.201.12.57'
+g_db_ip= '121.201.12.67'
 g_db_user= 'root'
 g_db_passwd = 'rjkj@rjkj'
 g_db_db = 'dnspro_core'
