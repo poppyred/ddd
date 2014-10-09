5aa3a29fd0ceb902a15d9d4cca09843f2981ae59
likunxiang <lkxlamborghini@126.com>
2014-10-09 10:05:24 +0800

    commit

--- rmgr/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# made by likunxiang
-
-#nohup python mgr.py > mgr.log 2>&1 &
--- rmgr/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# made by likunxiang
-
-ps -ef | grep python | grep mgr.py | grep -v grep | awk '{print $2}' | xargs kill -9
-ps -ef | grep python | grep mgr.py | grep -v grep
