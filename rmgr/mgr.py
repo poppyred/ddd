294f19f9224893737988e03fa617f29bc81a6f55
likunxiang <lkxlamborghini@126.com>
2014-10-29 17:37:16 +0800

    commit

--- rmgr/mgr.py
+++ rmgr/mgr.py
@@ -14,11 +14,9 @@ import mgr_err_describe
 
 def main_loop():
     while True:
-        msg.g_now += 1
-        time.sleep(1)
+        time.sleep(0.01)
         g_factory.get_repth_thread().check_event()
         g_factory.get_check_thread().check_event()
-
 
 if __name__ == '__main__':
     mgr_err_describe.g_err_desc = g_factory.get_err_info()
@@ -27,6 +25,7 @@ if __name__ == '__main__':
     g_factory.get_mgr_worker().set_buddy_thread(g_factory.get_http_thread(), g_factory.get_check_thread())
     g_factory.get_check_thread().add_tasknode_byinterval_lock(msg.g_class_inner_chk_task_db_heartbeat,
             mgr_conf.g_inner_chk_task_db_heartbeat)
+    g_factory.get_tick_thread().start()
     time.sleep(1)
     main_loop()
 
