b288c01bf31b4109532845904178da39977426b6
likunxiang <lkxlamborghini@126.com>
2015-01-13 20:59:48 +0800

    commit

--- ccc/AdminWeb/Home/Lib/Action/BaseAction.class.php
+++ ccc/AdminWeb/Home/Lib/Action/BaseAction.class.php
@@ -6,12 +6,27 @@ class BaseAction extends Action {
 		$user = $this->saveCurrentUserSession();
 		if (empty($user)) {
 			$actionName = strtolower(ACTION_NAME);
-		if (!in_array($actionName, array("login", "checklogin"))) {
-			header("Location: ".__APP__."/Index/index");
-			exit;
-		} 
-	}
-	$this->user = $user;
+			if (!in_array($actionName, array("login", "checklogin"))) {
+				header("Location: ".__APP__."/Index/index");
+				exit;
+			}
+		}	
+		$url = __ACTION__;
+		//监控权限
+		if($_SESSION['level'] == 0 ){
+			if(strstr($url,"/FlowChart") || strstr($url,"/System")){
+			}else{
+				header("Location: ".__APP__."/FlowChart");	
+			}
+		}
+		//客户支撑权限
+		if($_SESSION['level'] == 2 ){
+			if(strstr($url,"/Clientmgr") || strstr($url,"/Domainmgr") || strstr($url,"/ReverseDomain")){
+			}else{
+				header("Location: ".__APP__."/Clientmgr");	
+			}
+		}
+		$this->user = $user;
 	}
 	function saveCurrentUserSession() {
 		return isset($_SESSION['admin']) ? $_SESSION['admin'] :null;
---
+++ mgr/dbback.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mysqldump -h121.201.12.58 -uroot -prjkj@rjkj --opt -RB dnspro_core |gzip -9 > /media/wdata/linuxshare/db_backup/dnspro_core58-`date +%Y-%m-%d_%H%M%S`.sql.gz
+mysqldump -h121.201.12.68 -uroot -prjkj@rjkj --opt -RB dnspro_core |gzip -9 > /media/wdata/linuxshare/db_backup/dnspro_core68-`date +%Y-%m-%d_%H%M%S`.sql.gz
+
+/bin/rm -rf /media/wdata/linuxshare/db_backup/dnspro_core58-`date '+%Y-%m-%d'`*
+/bin/rm -rf /media/wdata/linuxshare/db_backup/dnspro_core68-`date '+%Y-%m-%d'`*
